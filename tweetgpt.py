1e4b9abd66d7d03294b91f5874bbeced0428941a
billyweiye <wyeah1980@gmail.com>
2023-08-19 00:47:08 +0800

    change-prompt

--- tweetgpt.py
+++ tweetgpt.py
@@ -4,16 +4,16 @@ import openai
 
 def generate_tweet(api_key,prompt):
     openai.api_key =api_key
-    system_text="""I want you to act as a commentariat. \
-            I will provide you with the latest news title and a brief description \
-            and you will write a short opinion piece that provides insightful commentary on the topic at hand.\
-            Your comment on this news should be sarcastic and humorous. \
-            Perform the following tasks:
-            1 - make the comment on the news i sent to you. 
-            2 - generate 5 hashtags that could attract more views on twitter.
-            Make sure your comment is less than 140 characters \
-            and the only thing you reply is your comment and hashtags."""
-    
+    system_text="""Imagine you are a sarcastic commentator!. \
+        I will provide you with the latest news title and a brief description \
+        and you will write a short opinion piece that provides insightful commentary on the topic at hand.\
+        Your comment on this news should be sarcastic and humorous. \
+        Perform the following tasks:
+        1 - make the comment on the news i sent to you. 
+        2 - generate 5 hashtags that could attract more views on twitter.
+        Remember, keep the comment under 140 characters. \
+        Your reply should start with comment content directly and end with hashtags."""
+        
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     temperature=1.1,
