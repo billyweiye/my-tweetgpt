2ea28da69e32f90e0b1c25c5c7af9f08413891a4
billyweiye <wyeah1980@gmail.com>
2023-08-20 00:13:08 +0800

    'add-thread-cn'

--- index.py
+++ index.py
@@ -1,35 +1,41 @@
 import schedule
-import requests
 import configparser
 import datetime
+import pytz
 import time
 from tweetgpt import generate_tweet
 from tweet import post_tweet 
 import logging 
 from news import get_headlines
+import threading
 
+# Counter to keep track of job executions
+job_execution_count_initial = 0
+max_job_executions = 10
+news_posted=[]
 # Counter to keep track of job executions
 # 创建线程本地存储对象
 local_data = threading.local()
 
 # 设置线程本地存储中的全局变量字典
-def set_global_vars(vars_dict):
-    local_data.global_vars = vars_dict
+def set_job_execution_count(job_execution_count=0):
+    local_data.job_execution_count = job_execution_count
+    
 
 # 获取线程本地存储中的全局变量字典
-def get_global_vars():
-    if not hasattr(local_data, 'global_vars'):
-        return {}
-    return local_data.global_vars
+def get_job_execution_count():
+    if not hasattr(local_data, 'job_execution_count'):
+        local_data.job_execution_count = job_execution_count_initial
+    return local_data.job_execution_count
 
 def reset_job_counter():
-    global_vars = get_global_vars()
-    global_vars[threading.current_thread().name].get("job_execution_count") =0
+    set_job_execution_count(job_execution_count=job_execution_count_initial)
     logging.info("Job execution count reset at", datetime.datetime.now())
 
-def main(country,language,timezone,job_execution_count,max_job_executions):
-    global job_execution_count,news_posted
-    now = datetime.datetime.now()
+def main(country,language,timezone):
+    global news_posted
+    job_execution_count=get_job_execution_count()
+    current_time = datetime.datetime.now()
     # 将当前时间设置为目标时区的时间
     target_time = current_time.astimezone(timezone)
     # 限制任务时间 
@@ -52,7 +58,7 @@ def main(country,language,timezone,job_execution_count,max_job_executions):
             if news_title not in news_posted :
                 break
             elif cnt>=max_job_executions:
-                job_execution_count=max_job_executions
+                set_job_execution_count(max_job_executions)
                 return
             else:
                 cnt+=1
@@ -71,9 +77,10 @@ def main(country,language,timezone,job_execution_count,max_job_executions):
 
         logging.info(f"{job_execution_count}",'||',tweets)
 
-        post_tweet(auth=auth,text=tweets)
+        if tweets:
+            post_tweet(auth=auth,text=tweets)
 
-        job_execution_count += 1
+        set_job_execution_count(job_execution_count + 1)
 
 
 
@@ -87,8 +94,8 @@ def schedule_job():
     scheduler_us.every().day.at("00:00").do(reset_job_counter)
 
     # Schedule the job to run every 10 to 48 minutes between 8 am and 10 pm
-    scheduler_cn.every(10).to(25).minutes.do(main,country='cn',timezone=cn_timezone,job_execution_count=job_execution_count_cn,)
-    scheduler_us.every(10).to(25).minutes.do(main,country='us',timezone=us_timezone)
+    scheduler_cn.every(10).to(25).minutes.do(main,country='cn',timezone=cn_timezone,language="Simplified Chinese")
+    scheduler_us.every(10).to(25).minutes.do(main,country='us',timezone=us_timezone,language='English')
 
     # 定义一个函数来运行调度器
     def run_scheduler(scheduler):
@@ -101,23 +108,17 @@ def schedule_job():
                 time.sleep(300)
 
     # 创建两个线程来并行执行任务
-    thread1 = threading.Thread(target=run_scheduler, args=(scheduler1,))
-    thread2 = threading.Thread(target=run_scheduler, args=(scheduler2,))
+    thread_cn = threading.Thread(target=run_scheduler, args=(scheduler_cn,))
+    thread_us = threading.Thread(target=run_scheduler, args=(scheduler_us,))
     
     # 启动线程
-    thread1.start()
-    thread2.start()
+    thread_cn.start()
+    thread_us.start()
 
     # 等待线程结束
-    thread1.join()
-    thread2.join()
+    thread_cn.join()
+    thread_us.join()
 
-
-# 定义一个函数来运行调度器
-def run_scheduler(scheduler):
-    while True:
-        scheduler.run_pending()
-        time.sleep(1)
 
 
 if __name__ == "__main__":
@@ -148,13 +149,6 @@ if __name__ == "__main__":
 
     cn_timezone = pytz.timezone('Asia/Shanghai')
 
-    #initialize global vars
-    global_vars={
-        'news_posted':[],
-        'job_execution_count':0,
-        'max_job_executions_cn':20
-    }
-    set_global_vars(global_vars)
 
     schedule_job()
 
