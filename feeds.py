f8c9f3dd8506026cbf12ebf93af56e240736fc0c
billyweiye <wyeah1980@gmail.com>
2023-09-10 19:51:48 +0800

    'change-to-rss-sources'

---
+++ feeds.py
@@ -0,0 +1,71 @@
+import feedparser 
+from bs4 import BeautifulSoup
+import time
+import xml.etree.ElementTree as ET
+
+
+
+def get_rss_urls(opml_file_path='Feeds.opml',category=[]):
+
+    tree = ET.parse(opml_file_path)
+    root = tree.getroot()
+
+    body = root.find('body')
+
+    opml_data = []  # 用于存储字典格式的OPML数据
+
+    for outline in body.findall('outline'):
+        if outline.get("title") in category or not category:
+            # 提取outline元素的属性，例如text、type、xmlUrl等
+            for line in outline.findall("outline"):
+                outline_dict = {
+                    'text': line.get('text'),
+                    'type': line.get('type'),
+                    'xmlUrl': line.get('xmlUrl')
+                    # 添加其他属性，如果有的话
+                }
+                
+                # 将每个outline元素的属性字典添加到opml_data列表中
+                opml_data.append(outline_dict)
+
+    # 现在opml_data包含了字典格式的OPML数据
+    # 您可以根据需要进一步处理或访问这些数据
+    return opml_data
+
+
+def get_feeds(rss_source:list=[],publish_limit:int = 60):
+    waitlist=[]
+    for rss in rss_source:
+        rss_url=rss.get("xmlUrl")
+        if rss_url:
+            response = feedparser.parse(rss_url)
+            for entry in response.get('entries',[]):
+                if time.mktime(entry.get("published_parsed",time.strptime("2020-09-09 19:00:00", "%Y-%m-%d %H:%M:%S")))< time.mktime(time.gmtime())-publish_limit*60: #只看publish_limit分钟内发布的内容
+                    print(f"{rss.get('text')}:out-to-date news")
+                    break
+                elif entry.get("id") not in waitlist:
+                    summary_raw = entry.get("summary")
+                    if summary_raw:
+                        # 使用Beautiful Soup解析HTML
+                        soup = BeautifulSoup(summary_raw, 'html.parser')
+
+                        # 查找<div>标签
+                        div_tag = soup.find('div')
+
+                        # 提取<div>中的文本内容
+                        summary_text = div_tag.text if div_tag else entry.get("title")
+
+                    waitlist.append({
+                        "id":entry.get("id"),
+                        "title":entry.get("title"),
+                        "link":entry.get("link"),
+                        "date":entry.get("published_parsed"),
+                        "description":summary_text
+                    })
+
+                    print(f"{rss.get('text')} \n {entry.get('title')}")
+                    break
+                else:
+                    break
+    sorted_waitlist=sorted(waitlist,key=lambda x: x['date'],reverse=True)
+    return sorted_waitlist
--- index.py
+++ index.py
@@ -8,15 +8,12 @@ from tweet import post_tweet
 import logging 
 from news import newsdata
 import threading
+import queue
+from feeds import get_rss_urls,get_feeds
+import random
 
-# Counter to keep track of job executions
-job_execution_count_initial = 0
-max_job_executions = 30
-news_posted=[]
-# Counter to keep track of job executions
 # 创建线程本地存储对象
 local_data = threading.local()
-
 # 设置线程本地存储中的全局变量字典
 def set_job_execution_count(job_execution_count=0):
     local_data.job_execution_count = job_execution_count
@@ -25,121 +22,126 @@ def set_job_execution_count(job_execution_count=0):
 # 获取线程本地存储中的全局变量字典
 def get_job_execution_count():
     if not hasattr(local_data, 'job_execution_count'):
-        local_data.job_execution_count = job_execution_count_initial
+        local_data.job_execution_count = 0
     return local_data.job_execution_count
 
-def save_news_list(news_list):
-    local_data.news_list = news_list
-    
-
-# 获取线程本地存储中的全局变量字典
-def get_news_list():
-    if not hasattr(local_data, 'news_list'):
-        local_data.news_list = ""
-    return local_data.news_list
-
 def reset_job_counter():
-    set_job_execution_count(job_execution_count=job_execution_count_initial)
+    set_job_execution_count(job_execution_count=0)
     logging.info(f"Job execution count reset at {datetime.datetime.now()}")
 
-def main(country,language,timezone):
-    global news_posted
-    job_execution_count=get_job_execution_count()
-    current_time = datetime.datetime.now()
-    # 将当前时间设置为目标时区的时间
-    target_time = current_time.astimezone(timezone)
-    # 限制任务时间 
-    if target_time.hour < 7 or target_time.hour > 23:
-        return
 
-    if job_execution_count < max_job_executions:
-        logging.info(f"TASK: {job_execution_count}")
+    
 
-        #get news list
-        # kw='China'
-        # sort_type = "relevancy"
-        # news=get_news(topic=kw, date=date, sort_type=sort_type, apikey=apikey)
+# 获取及储存线程本地存储中的已发布的feeds
+def get_posted_news():
+    if not hasattr(local_data, 'posted_news'):
+        local_data.posted_news = []
+    return local_data.posted_news
 
-       # news=get_headlines(country=country,category='',topic='',apikey=apikey)
-        news=get_news_list()
-        cnt=0
-        while True:
-            if news:
-                news_title=news[cnt].get("title")
-                if news_title not in news_posted :
-                    break
-                elif cnt>=len(news)-1:
-                    news=newsdata(apikey,timeframe=3,language="en",q="Politics OR Finance OR Stock Market OR Technology OR Science")
-                    save_news_list(news)
-                    cnt=0
-                    time.sleep(60)
-                else:
-                    cnt+=1
-                    time.sleep(0.8)
+def save_posted_news(news_id):
+    local_data.posted_news.append(news_id) 
+
+
+
+my_queue = queue.Queue()  # 使用优先级队列
+def put_unique_message(queue_obj, message):
+    # 检查队列中是否已经存在相同的消息
+    if message not in queue_obj.queue:
+        queue_obj.put(message)
+        logging.info(f"Inserted unique message: {message}")
+    else:
+        logging.info(f"Message already exists in the queue: {message}")
+
+def news_queue(mins_interval:int =1,catogery:list = [],publish_time:int=60):
+    while True:
+        try:
+            urls=get_rss_urls("Feeds.opml",catogery)
+            news=get_feeds(urls,publish_time)
+            for feed in news:
+                put_unique_message(my_queue,feed)
+            logging.info(f"The Current Queue: {my_queue.queue}")
+            time.sleep(mins_interval*60)
+        except Exception as e:
+            logging.error(f"Error occured in {threading.current_thread()}: {e}")
+
+
+def get_feed_queue():
+    return my_queue.get()
+
+
+
+def tweet_job(min_tweet_interval,max_tweet_interval,language,timezone):
+    while True:
+        try:
+            job_execution_count=get_job_execution_count()
+
+            current_time = datetime.datetime.now()
+            # 将当前时间设置为目标时区的时间
+            target_time = current_time.astimezone(timezone)
+            # 限制任务时间 
+            if target_time.hour < 7 or target_time.hour > 23:
+                logging.info("Not Scheduled time. Wait another 10 mins.")
+                time.wait(10*60)   #等待10分钟
+                continue
+
+            if job_execution_count < max_job_executions:
+                logging.info(f"TASK: {job_execution_count}")
+
+                news=get_feed_queue()
+                news_id=news.get("id")
+                if news_id in get_posted_news() :
+                    time.sleep(1) 
+                    continue
+
+                save_posted_news(news_id)
+                news_title=news.get("title")
+                new_description=news.get("description")
+                news_url=news.get("link")
+
+                #调用GPT 生成content
+                prompts=f"title:{news_title} || description:{new_description}"
+                logging.info(f"Prompts:{prompts}")
+                tweets=generate_tweet(openai_api_key,prompts,language)
+                
+                #加上news link
+                tweets += f" {news_url}"
+
+                logging.info(f"TASK: {job_execution_count} || {tweets}")
+
+                if tweets:
+            #        post_tweet(auth=auth,text=tweets)
+                    pass
+
+                set_job_execution_count(job_execution_count + 1)
+
+                
+                time.sleep(random.randint(min_tweet_interval*60,max_tweet_interval*60))
             else:
-                news=newsdata(apikey,timeframe=2,language="en",q="Politics OR Finance OR Stock Market OR Technology OR Science")
-                save_news_list(news)
-                cnt=0
-                time.sleep(0.8)
-
-        news_posted.append(news_title)
-       # news_title=news.get("articles")[job_execution_count+cnt].get("title")
-        new_description=news[cnt].get("abstract")
-        news_url=news[cnt].get("link")
-
-        prompts=f"title:{news_title} || description:{new_description}"
-
-        tweets=generate_tweet(openai_api_key,prompts,language)
-
-        tweets += f" {news_url}"
-
-        logging.info(f"TASK: {job_execution_count} || {tweets}")
-
-        if tweets:
-            post_tweet(auth=auth,text=tweets)
-
-        set_job_execution_count(job_execution_count + 1)
+                time.sleep(30*60)
+        except Exception as e:
+            logging.error(f"Error occured in {threading.current_thread()}: {e}")
 
 
 
-def schedule_job():
-    # 创建任务和调度器
- #   scheduler_cn = schedule.Scheduler()
-    scheduler_us = schedule.Scheduler()
-
-    # Schedule the job to reset the counter every day at midnight
- #   scheduler_cn.every().day.at("00:00",us_timezone).do(reset_job_counter)
-    scheduler_us.every().day.at("00:00",us_timezone).do(reset_job_counter)
-
-    # Schedule the job to run every 10 to 48 minutes between 8 am and 10 pm
-    min_interval=5
-    max_interval=30
- #   scheduler_cn.every(5).to(40).minutes.do(main,country='cn',timezone=cn_timezone,language="Simplified Chinese")
-    scheduler_us.every(min_interval).to(max_interval).minutes.do(main,country='us',timezone=us_timezone,language='English')
-
-    # 定义一个函数来运行调度器
-    def run_scheduler(scheduler):
-        while True:
-            try:
-                logging.info(f"{threading.current_thread().name},Next Run at: {scheduler.next_run}")
-                scheduler.run_pending()
-                time.sleep(min_interval*6)  # Sleep for a second to avoid high CPU usage
-            except Exception as e:
-                logging.error("An unexpected error occurred: %s", e)
-                time.sleep(300)
+def schedule_job(news_req_interval:int=1,category:list=[],publish_time:int=60,min_tweet_interval:int=1,max_tweet_interval:int=10,language_to_tweet:str="English",timezone=pytz.timezone('America/New_York')):
+    #创建获取feeds线程
+    thread_rss=threading.Thread(target=news_queue,args=(news_req_interval,category,publish_time),name="RSS")
 
     # 创建两个线程来并行执行任务
- #   thread_cn = threading.Thread(target=run_scheduler, args=(scheduler_cn,),name="CHINA")
-    thread_us = threading.Thread(target=run_scheduler, args=(scheduler_us,),name="USA")
-    threading.current_thread()
+    thread_tweet = threading.Thread(target=tweet_job,args=(min_tweet_interval,max_tweet_interval,language_to_tweet,us_timezone), name="TWEET")
     
     # 启动线程
-#    thread_cn.start()
-    thread_us.start()
+    thread_rss.start()
+    thread_tweet.start()
+    
 
     # 等待线程结束
-#    thread_cn.join()
-    thread_us.join()
+    thread_rss.join()
+    thread_tweet.join()
+
+    #创建定时任务来重置发推次数上限
+    scheduler = schedule.Scheduler()
+    scheduler.every().day.at("00:00",us_timezone).do(reset_job_counter)
 
 
 
@@ -155,8 +157,6 @@ if __name__ == "__main__":
     config = configparser.ConfigParser()
     config.read('config.ini')
 
-    apikey=config['news']['newsdata_key']
-
     openai_api_key=config['openai']["openai_api_key"]
 
     auth={
@@ -166,13 +166,24 @@ if __name__ == "__main__":
             "access_token_secret" :config['twitter']["token_secret"] ,
     }
 
+    # Counter to keep track of job executions
+    max_job_executions = 45
+
     # 指定目标时区
     us_timezone = pytz.timezone('America/New_York')
 
     cn_timezone = pytz.timezone('Asia/Shanghai')
 
+    new_category=["Tech"]
+    news_req_interval=1  #每1分钟检查一次rss
+    publish_time_limt=60
+    min_tweet_interval=1.5 
+    max_tweet_interval=5
+    language_to_tweet="English"
+    rss_category=["Tech"]
 
-    schedule_job()
+
+    schedule_job(news_req_interval,rss_category,publish_time_limt,min_tweet_interval,max_tweet_interval,language_to_tweet,us_timezone)
 
     
 
