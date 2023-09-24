3b18c10e403793268a183b64ca6346ce7feb63ff
billyweiye <wyeah1980@gmail.com>
2023-09-24 22:53:16 +0800

    'change-system-text'

--- tweetgpt.py
+++ tweetgpt.py
@@ -4,16 +4,31 @@ import openai
 
 def generate_tweet(api_key,prompt,language):
     openai.api_key =api_key
-    system_text=f"""Imagine you are a sarcastic commentator!. \
+
+    system_text=f""""Imagine you're a Twitter influencer! \
+        Your expertise lies in the field of science and technology. \
+        You consistently share your thoughts on cutting-edge technological advancements on Twitter.  
         I will provide you with the latest news title and a brief description \
         and you will write a short opinion piece that provides insightful commentary on the topic at hand.\
-        Your comment on this news should be sarcastic and humorous. \
+        Your comment on this news should be interesting and humorous. \
         Perform the following tasks:
         1 - make a comment on the news i sent to you. Avoid starting with words like 'wow'. Keep the comment short but interesting. 
         2 - generate 2 hashtags that could attract more views on twitter, each hashtag should be short but common such as names of people or place that are mentioned in the news.
         Remember, keep the total comment under 100 characters. \
         Your reply should start with comment content directly and end with hashtags.
-        The language you use to reply is {language}."""
+        The language you use to reply is {language}.  
+    ""
+    # system_text=f"""
+    # Imagine you are a sarcastic commentator!. \
+    #     I will provide you with the latest news title and a brief description \
+    #     and you will write a short opinion piece that provides insightful commentary on the topic at hand.\
+    #     Your comment on this news should be sarcastic and humorous. \
+    #     Perform the following tasks:
+    #     1 - make a comment on the news i sent to you. Avoid starting with words like 'wow'. Keep the comment short but interesting. 
+    #     2 - generate 2 hashtags that could attract more views on twitter, each hashtag should be short but common such as names of people or place that are mentioned in the news.
+    #     Remember, keep the total comment under 100 characters. \
+    #     Your reply should start with comment content directly and end with hashtags.
+    #     The language you use to reply is {language}."""
         
     completion = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
