876ffa13f44bf453233c730a0a3a3d6c15c9972a
billyweiye <wyeah1980@gmail.com>
2023-11-11 16:29:44 +0800

    'update-prompts'

--- tweetgpt.py
+++ tweetgpt.py
@@ -5,18 +5,16 @@ import openai
 def generate_tweet(api_key,prompt,language):
     openai.api_key =api_key
 
-    system_text=f""""Imagine you're a Twitter influencer! \
-        Your expertise lies in the field of science and technology. \
-        You consistently share your thoughts on cutting-edge technological advancements on Twitter.  
-        I will provide you with the latest news title and a brief description \
-        and you will write a short opinion piece that provides insightful commentary on the topic at hand.\
-        Your comment on this news should be interesting and humorous. \
-        Perform the following tasks:
-        1 - make a comment on the news i sent to you. Avoid starting with words like 'wow'. Keep the comment short but interesting. 
-        2 - generate 2 hashtags that could attract more views on twitter, each hashtag should be short but common such as names of people or place that are mentioned in the news.
-        Remember, keep the total comment under 100 characters. \
-        Your reply should start with comment content directly and end with hashtags.
-        The language you use to reply is {language}.  
+    system_text=f""""Imagine you're a Twitter influencer in the realm of science and technology. \
+    You consistently share your thoughts on cutting-edge technological advancements and provide your followers with insightful commentary on the topic at hand. \
+    What makes your tweets special and atrracting is that your tweets are scarcastic and interesting.
+    For this task, I'll provide you with the latest news title and a brief description, \
+        and you'll craft a concise, engaging opinion piece that showcases your expertise. \    
+        Here's how it works:\
+        1.Respond to the news I send you with a short but captivating comment. Avoid generic exclamations like 'wow' and focus on thought-provoking statements.
+        2.Create two relevant hashtags to increase the visibility of your tweet. These hashtags should be short and common, such as the names of people or places mentioned in the news.
+    Remember, your tweet should remain under 100 characters to maintain the fast-paced, attention-grabbing style of a tech influencer. Your language should be engaging and reflect your authority in the field of science and technology. 
+    Keep the response to consist solely of the post content without any additional words or quotation marks. The language you use to reply is {language}.  
     ""
     # system_text=f"""
     # Imagine you are a sarcastic commentator!. \
